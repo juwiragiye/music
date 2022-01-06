6b3fe928d3daa75fce874f8e330cf11eee82c73c
juwiragiye <uwiragiyejanvi@gmail.com>
2022-01-06 12:49:51 -0500

    sidebar menu refactor

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -6,62 +6,17 @@ import {
   RefreshIcon,
   SaveIcon,
   GlobeAltIcon,
-  PlusCircleIcon,
 } from "@heroicons/react/outline";
 
 import { HeartIcon } from "@heroicons/react/solid";
 import { MdPlaylistAdd } from "react-icons/md";
+import { NavLink } from "remix";
 
 export function Sidebar() {
   return (
     <aside className="w-36 flex flex-col justify-center text-slate-500 px-5  pt-10 h-screen fixed overflow-clip">
-      <p className="mb-2 ml-1.5 font-semibold uppercase text-xs tracking-wider  ">
-        Menu
-      </p>
-      <div className=" flex flex-col items-center  space-y-4 px-2  b-red-400">
-        <div className="flex w-full  mr-2 shadow-md items-center space-x-2 cursor-pointer text-sm bg-gray-100 py-1 px-2 rounded-md">
-          <HomeIcon className="h-3 text-indigo-500" />
-          <p>Home</p>
-        </div>
-        <div className="flex  text-sm  w-full  items-center space-x-2 cursor-pointer">
-          <SearchIcon className="h-3 text-indigo-500" />
-          <p>Browse</p>
-        </div>
-        <div className="flex text-sm  w-full  items-center  space-x-2 cursor-pointer">
-          <UserIcon className="h-3 text-indigo-500" />
-          <p>Artits</p>
-        </div>
-        <div className="flex  text-sm  w-full  items-center space-x-2 cursor-pointer">
-          <CollectionIcon className="h-3 text-indigo-500" />
-          <p>Albums</p>
-        </div>
-        <div className="flex  text-sm  w-full  items-center space-x-2 cursor-pointer">
-          <GlobeAltIcon className="h-3 text-indigo-500" />
-          <p>Genres</p>
-        </div>
-      </div>
-
-      <p className="mb-2 ml-1.5 mt-10 font-semibold uppercase text-xs tracking-wider">
-        Library
-      </p>
-      <div className=" flex flex-col items-center  space-y-4 px-2  b-red-400">
-        <div className="flex  text-sm  w-full  items-center space-x-2 cursor-pointer">
-          <SaveIcon className="h-3 text-indigo-500" />
-          <p>Playlist</p>
-        </div>
-        <div className="flex  text-sm  w-full  items-center space-x-2 cursor-pointer">
-          <RefreshIcon className="h-3 text-indigo-500" />
-          <p>Recents</p>
-        </div>
-        <div className="flex text-sm  w-full  items-center  space-x-2 cursor-pointer">
-          <UserIcon className="h-3 text-indigo-500" />
-          <p>Artits</p>
-        </div>
-        <div className="flex  text-sm  w-full  items-center space-x-2 cursor-pointer">
-          <HeartIcon className="h-3 text-indigo-500" />
-          <p>Favourites</p>
-        </div>
-      </div>
+      <Menu />
+      <Library />
       <p className="mb-2 ml-1.5 mt-10 font-semibold uppercase text-xs tracking-wider">
         Playlists
       </p>
@@ -93,5 +48,81 @@ export function Sidebar() {
         <p>New Playlist</p>
       </button>
     </aside>
+  );
+}
+
+export function Menu() {
+  return (
+    <>
+      <p className="mb-2 ml-1.5 font-semibold uppercase text-xs tracking-wider  ">
+        Menu
+      </p>
+      <div className="flex flex-col items-center space-y-4 px-2">
+        <NavLink
+          to="/login"
+          className="flex  text-sm  w-full  items-center space-x-2 cursor-pointer  sm:py-1 sm:px-2 rounded-md sm:mr-2 sm:bg-gray-100 sm:shadow-md"
+        >
+          <HomeIcon className="h-6 text-indigo-500 sm:h-3" />
+          <span className="text-indigo-50  w-4  sm:text-gray-500">Home</span>
+        </NavLink>
+        <NavLink
+          to="/"
+          className="flex text-sm w-full  items-center space-x-2 cursor-pointer"
+        >
+          <SearchIcon className="h-6 text-indigo-500 sm:h-3" />
+          <span className="text-indigo-50  w-4  sm:text-gray-500">Browse</span>
+        </NavLink>
+
+        <NavLink
+          to="/"
+          className="flex text-sm  w-full  items-center  space-x-2 cursor-pointer"
+        >
+          <UserIcon className="h-6 text-indigo-500 sm:h-3" />
+          <span className="text-indigo-50  w-4  sm:text-gray-500">Artits</span>
+        </NavLink>
+        <NavLink
+          to="/"
+          className="flex  text-sm  w-full  items-center space-x-2 cursor-pointer"
+        >
+          <CollectionIcon className="h-6 text-indigo-500 sm:h-3" />
+          <span className="text-indigo-50  w-4  sm:text-gray-500">Albums</span>
+        </NavLink>
+        <NavLink
+          to="/"
+          className="flex  text-sm  w-full  items-center space-x-2 cursor-pointer"
+        >
+          <GlobeAltIcon className="h-6 text-indigo-500 sm:h-3" />
+          <span className="text-indigo-50  w-4  sm:text-gray-500">Genres</span>
+        </NavLink>
+      </div>
+    </>
+  );
+}
+
+function Library() {
+  return (
+    <>
+      <p className="mb-2 ml-1.5 mt-10 font-semibold uppercase text-xs tracking-wider">
+        Library
+      </p>
+      <div className=" flex flex-col items-center  space-y-4 px-2  b-red-400">
+        <div className="flex  text-sm  w-full  items-center space-x-2 cursor-pointer">
+          <SaveIcon className="h-3 text-indigo-500" />
+          <p>Playlists</p>
+        </div>
+        <div className="flex  text-sm  w-full  items-center space-x-2 cursor-pointer">
+          <RefreshIcon className="h-3 text-indigo-500" />
+          <p>Recents</p>
+        </div>
+        <div className="flex text-sm  w-full  items-center  space-x-2 cursor-pointer">
+          <UserIcon className="h-3 text-indigo-500" />
+          <p>Artits</p>
+        </div>
+        <div className="flex  text-sm  w-full  items-center space-x-2 cursor-pointer">
+          <HeartIcon className="h-3 text-indigo-500" />
+          <p>Favourites</p>
+        </div>
+      </div>
+    </>
   );
 }
